40b1a1106a34c94a78efd782f8b1dd11c01b8d38
Javad <35368556+Javad-Khoshnevis@users.noreply.github.com>
2019-10-23 19:10:02 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+print ("\033c")
+print ("++++++++++    welcome to PyMath!    ++++++++++")
+print ("    Select your question : \n")
+print ("        1 - Is this number prime ?")
+print ("        2 - Comming soon ...\n")
+number = input ("        = ")
+
+if number == 1 :
+    execfile('prime-number.py')
+elif number == 2 :
+    print("this is not released !")
+else:
+    print("Invalid choice!")
